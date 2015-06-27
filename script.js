bbac0d11df94371da7548f99b81185636b5ae5ab
Michael Wheatley <ebirt@live.com>
2015-06-26 21:38:49 -0400

    updated website

--- script.js
+++ script.js
@@ -7,23 +7,31 @@ $(document).ready(function(){
 function getTxt()
 {
     $.get("http://miatribe.github.io/pipvpspy/Loki.txt?" + (new Date().getTime()), function(data){
-        parseServerTxt(data);
+        parseServerTxt(data, "Loki");
+    });
+    $.get("http://miatribe.github.io/pipvpspy/Chaos.txt?" + (new Date().getTime()), function(data){
+        parseServerTxt(data, "Chaos");
+    });
+    $.get("http://miatribe.github.io/pipvpspy/Thor.txt?" + (new Date().getTime()), function(data){
+        parseServerTxt(data, "Thor");
     });
 }
-function parseServerTxt (serverString)
+function parseServerTxt (serverString, name)
 {
     var firstSplit = serverString.split(',');
     var secondSplit = firstSplit[0].split(':');
     var total = 0;
-    $("#perMap").empty();
+    $("#perMap" + name).empty();
     $(secondSplit).each(function(index){
         total += parseInt(secondSplit[index].match(/\d+(?= \/)/g));
-        $("#perMap").append("<li>" + secondSplit[index] + "</li>");
+        $("#perMap" + name).append("<li>" + secondSplit[index] + "</li>");
     });
-    $("#secsago").data("update", Math.floor(firstSplit[1]));
-    $("#total span").empty().append(total);
+    $("#secsago" + name).data("update", Math.floor(firstSplit[1]));
+    $("#total"+ name + " span").empty().append(total);
 }
 function timeSinceUpdate()
 {
-    $("#secsago").html((Math.floor((new Date().getTime()) / 1000)) - parseInt($("#secsago").data("update")));
+    $("#secsagoLoki").html((Math.floor((new Date().getTime()) / 1000)) - parseInt($("#secsagoLoki").data("update")));
+    $("#secsagoChaos").html((Math.floor((new Date().getTime()) / 1000)) - parseInt($("#secsagoChaos").data("update")));
+    $("#secsagoThor").html((Math.floor((new Date().getTime()) / 1000)) - parseInt($("#secsagoThor").data("update")));
 }
